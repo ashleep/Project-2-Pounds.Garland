d65fdee7447bc6024ebd7682033712f8ae9fb859
ashleep <ashleelbs@gmail.com>
2020-05-12 21:24:33 -0700

    first pass

---
+++ query.sql
@@ -0,0 +1,9 @@
+-- Query to check successful load
+SELECT * FROM country_code;
+
+SELECT * FROM smoking_prevalence;
+
+SELECT * FROM life_expectancy;
+
+-- Create tables
+
---
+++ schema.sql
@@ -0,0 +1,38 @@
+-- Create Three Tables
+CREATE TABLE country_code (
+  	index INT PRIMARY KEY,
+  	country_code TEXT,
+  	country_name TEXT
+);
+
+CREATE TABLE smoking_prevalence (
+  	index INT PRIMARY KEY,
+  	sp_code TEXT,
+	sp_1990 REAL,
+	sp_2000 REAL,
+	sp_2011 REAL,
+	sp_2012 REAL,
+	sp_2013 REAL,
+	sp_2014 REAL,
+	sp_2015 REAL,
+	sp_2016 REAL,
+	sp_2017 REAL,
+	sp_2018 REAL,
+	sp_2019 REAL
+);
+
+CREATE TABLE life_expectancy (
+  	index INT PRIMARY KEY,
+  	le_code TEXT,
+	le_1990 REAL,
+	le_2000 REAL,
+	le_2011 REAL,
+	le_2012 REAL,
+	le_2013 REAL,
+	le_2014 REAL,
+	le_2015 REAL,
+	le_2016 REAL,
+	le_2017 REAL,
+	le_2018 REAL,
+	le_2019 REAL	
+);
