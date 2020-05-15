7550527516903c0d4a84bc7b37f76c9789a1b668
ashleep <ashleelbs@gmail.com>
2020-05-14 17:32:51 -0700

    comments added to jupyter notebook

--- query.sql
+++ query.sql
@@ -6,4 +6,12 @@ SELECT * FROM smoking_prevalence;
 SELECT * FROM life_expectancy;
 
 -- Create tables
+SELECT c.country_name, ROUND(avg(sp.sp_2011)) as "SP", ROUND(avg(le.le_2011)) as "LE"
+FROM smoking_prevalence sp
+JOIN country_code c
+ON c.country_code = sp.sp_code
+JOIN life_expectancy le
+ON c.country_code = le.le_code
+GROUP BY c.country_name;
+
 
