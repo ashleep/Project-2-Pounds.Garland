96b96196b733cb3c2be922c695eebf6beb27d71f
ashleep <ashleelbs@gmail.com>
2020-05-14 20:20:43 -0700

    final report:

--- query.sql
+++ query.sql
@@ -6,12 +6,20 @@ SELECT * FROM smoking_prevalence;
 SELECT * FROM life_expectancy;
 
 -- Create tables
-SELECT c.country_name, ROUND(avg(sp.sp_2011)) as "SP", ROUND(avg(le.le_2011)) as "LE"
+SELECT c.country_name, sp.sp_2000 as "Smoking Prevalence", le.le_2000 as "Life Expectancy"
 FROM smoking_prevalence sp
 JOIN country_code c
 ON c.country_code = sp.sp_code
 JOIN life_expectancy le
 ON c.country_code = le.le_code
-GROUP BY c.country_name;
+WHERE c.country_code = 'USA' OR  c.country_code = 'WLD';
+
+SELECT c.country_name, sp.sp_2016 as "Smoking Prevalence", le.le_2016 as "Life Expectancy"
+FROM smoking_prevalence sp
+JOIN country_code c
+ON c.country_code = sp.sp_code
+JOIN life_expectancy le
+ON c.country_code = le.le_code
+WHERE c.country_code = 'USA' OR  c.country_code = 'WLD';
 
 
